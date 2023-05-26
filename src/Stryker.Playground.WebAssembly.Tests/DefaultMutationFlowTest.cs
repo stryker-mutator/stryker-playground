e417a789df71ae957670d47404c0b39325618949
Rachid Boukaiba <rachidd@protonmail.com>
2023-05-26 02:15:17 +0200

    add e2e happy flow test

---
+++ src/Stryker.Playground.WebAssembly.Tests/DefaultMutationFlowTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Playwright;
+
+namespace Stryker.Playground.WebAssembly.Tests;
+
+public class DefaultMutationFlowTest
+{
+    [Fact]
+    public async Task VerifyMutationTestResult()
+    {
+        var playwright = await Playwright.CreateAsync();
+        var browser = await playwright.Chromium.LaunchAsync();
+        var page = await browser.NewPageAsync();
+        
+        await page.GotoAsync("https://localhost:7165/");
+        
+        // Wait for playground to initialize
+        await page.WaitForSelectorAsync("button:has-text(\"Run Mutation Tests\"):not([disabled])");
+        
+        // Run mutation tests
+        await page.ClickAsync("button:has-text(\"Run Mutation Tests\")");
+
+        // Wait for tests to finish & report to display. Then navigate back to editor screen
+        await page.WaitForSelectorAsync("button:has-text(\"Mutation Report\").active");
+        await page.ClickAsync("button:has-text(\"Editor\")");
+
+        // Validate terminal output
+        var terminalContent = await page.InnerTextAsync(".xterm-rows");
+        var expectedText = "Your mutation score is"
+            .Replace(" ", ""); // Strip spaces because InnerTextAsync does not preserve spaces from the xterm component..
+        
+        Assert.Contains(expectedText, terminalContent);
+    }
+}
